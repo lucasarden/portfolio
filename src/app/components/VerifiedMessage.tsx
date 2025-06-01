6084345febf53e3eff12b7c4bca95cf5a6c53f17
Lucas Arden <31015699+lucasarden@users.noreply.github.com>
2025-06-01 13:35:10 -0700

    Create VerifiedMessage.tsx

---
+++ src/app/components/VerifiedMessage.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export default function VerifiedMessage({ setSuccess }: { setSuccess: (msg: string) => void }) {
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (searchParams.get("verified") === "true") {
+      setSuccess("Your email has been verified. You can now log in.");
+    }
+  }, [searchParams, setSuccess]);
+
+  return null;
+}
