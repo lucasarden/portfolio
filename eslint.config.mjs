eaa16c894ed4576649824e68984ba1037ac0b4ca
Lucas <lucas91913@gmail.com>
2026-07-05 22:01:05 -0700

    updated next!

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,19 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  
-  ...compat.config({
-        rules: {
-            "@typescript-eslint/no-unused-vars": "off",
-            "no-unused-vars": "off",
-            "react/no-unescaped-entities": "off"
-        }
-    })
-];
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...compat.config({
+    rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+        "no-unused-vars": "off",
+        "react/no-unescaped-entities": "off"
+    }
+}), {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
